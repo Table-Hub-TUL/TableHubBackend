d375d6745c98d1b093bd887ddc11f4644646fe6a
Hubert Szadkowski <hubertszadkowski@protonmail.com>
2025-04-19 13:05:17 +0200

    Spring project with basic dependencies enabled

---
+++ th-backend/src/main/java/com/tablehub/thbackend/ThBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tablehub.thbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThBackendApplication.class, args);
+    }
+
+}
---
+++ th-backend/src/test/java/com/tablehub/thbackend/ThBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tablehub.thbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
