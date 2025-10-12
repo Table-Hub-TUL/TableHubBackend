bbed8611bfd19bf4c7bf7c4748c8906b91c2936a
Jakub Raj <kubaraj24@gmail.com>
2025-10-12 22:58:50 +0200

    adding @Email

--- th-backend/src/main/java/com/tablehub/thbackend/dto/request/AuthSignUpRequest.java
+++ th-backend/src/main/java/com/tablehub/thbackend/dto/request/AuthSignUpRequest.java
@@ -1,5 +1,6 @@
 package com.tablehub.thbackend.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -18,7 +19,9 @@ public class AuthSignUpRequest {
     private String password;
 
     @NotBlank
+    @Email
     private String email;
+
     @NotBlank
     private String nickname;
 }
