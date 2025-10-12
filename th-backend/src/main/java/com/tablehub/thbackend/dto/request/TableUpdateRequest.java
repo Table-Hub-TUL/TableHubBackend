97956762fb7aba42abaa138ee640dd0a8908e4fa
Sanc1i <60852731+Sanc1i@users.noreply.github.com>
2025-10-12 23:03:51 +0200

    Jr/th 30/autorization (#27)
    * auth fix
    
    * fixing 401 error (adding global exception handler)
    
    * adding @Email

--- th-backend/src/main/java/com/tablehub/thbackend/dto/request/TableUpdateRequest.java
+++ th-backend/src/main/java/com/tablehub/thbackend/dto/request/TableUpdateRequest.java
@@ -1,12 +1,17 @@
 package com.tablehub.thbackend.dto.request;
 
 import com.tablehub.thbackend.model.TableStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TableUpdateRequest {
+    @NotNull
     private Long restaurantId;
+    @NotNull
     private Long sectionId;
+    @NotNull
     private Long tableId;
+    @NotNull
     private TableStatus requestedStatus;
 }
--- th-backend/src/main/java/com/tablehub/thbackend/dto/request/UpdateTableStatusRequest.java
+++ th-backend/src/main/java/com/tablehub/thbackend/dto/request/UpdateTableStatusRequest.java
@@ -1,15 +1,21 @@
 package com.tablehub.thbackend.dto.request;
 
 import com.tablehub.thbackend.model.TableStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
 public class UpdateTableStatusRequest {
+    @NotNull
     private Long restaurantId;
+    @NotNull
     private Long sectionId;
+    @NotNull
     private Long tableId;
+    @NotNull
     private TableStatus requestedStatus;
 
 }
