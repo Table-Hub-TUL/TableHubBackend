c6c95493ec06ed3c7506079419fbce7c3c05823b
Sanc1i <60852731+Sanc1i@users.noreply.github.com>
2025-10-13 14:07:10 +0200

    adding get particular restaurant (#28)
    * adding get particular restaurant
    
    * delete useless files
    
    * conflicts resolved

--- th-backend/src/main/java/com/tablehub/thbackend/dto/SectionDto.java
+++ th-backend/src/main/java/com/tablehub/thbackend/dto/SectionDto.java
@@ -1,11 +1,20 @@
 package com.tablehub.thbackend.dto;
 
+import com.tablehub.thbackend.model.RestaurantSection;
+import com.tablehub.thbackend.model.SectionName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SectionDto {
     private Long id;
-    private String name;
+    private SectionName name;
+
+    public SectionDto(RestaurantSection section) {
+        this.id = section.getId();
+        this.name = section.getName();
+    }
 }
