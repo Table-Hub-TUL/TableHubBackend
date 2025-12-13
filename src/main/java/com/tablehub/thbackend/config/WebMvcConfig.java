ec85e803b2383d5dfdad74b40620ec6632944d87
Hubert Szadkowski <136641445+Koorbik@users.noreply.github.com>
2025-12-13 20:10:02 +0100

    CMS Reward Look improvement (#53)

--- src/main/java/com/tablehub/thbackend/config/WebMvcConfig.java
+++ src/main/java/com/tablehub/thbackend/config/WebMvcConfig.java
@@ -3,12 +3,15 @@ package com.tablehub.thbackend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String resourcePath = Paths.get("/media").toUri().toString();
+
         registry.addResourceHandler("/media/**")
-                .addResourceLocations("file:/media/");
+                .addResourceLocations(resourcePath);
     }
 }
