3439ff0c16316801e9e4e42502575d7919a22ee6
Jakub Raj <kubaraj24@gmail.com>
2026-01-10 18:11:49 +0100

    asdf

--- src/main/java/com/tablehub/thbackend/dto/response/RestaurantSimpleResponse.java
+++ src/main/java/com/tablehub/thbackend/dto/response/RestaurantSimpleResponse.java
@@ -1,14 +1,12 @@
 package com.tablehub.thbackend.dto.response;
 
-
 import com.tablehub.thbackend.model.*;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Getter
 @AllArgsConstructor
@@ -20,8 +18,10 @@ public class RestaurantSimpleResponse {
     private Address address;
     private Location location;
     private Double rating = 0.0;
+    private Integer freeTableCount;
+    private Integer totalTableCount;
 
-    public  RestaurantSimpleResponse(Restaurant restaurant) {
+    public RestaurantSimpleResponse(Restaurant restaurant) {
         this.id = restaurant.getId();
         this.name = restaurant.getName();
         this.cuisine = List.of(restaurant.getCuisineName());
@@ -30,5 +30,19 @@ public class RestaurantSimpleResponse {
             this.location = new Location(restaurant.getLocation().getY(), restaurant.getLocation().getX());
         }
         this.rating = restaurant.getRating();
+
+        if (restaurant.getSections() != null) {
+            this.freeTableCount = (int) restaurant.getSections().stream()
+                    .flatMap(section -> section.getTables() != null ? section.getTables().stream() : Stream.empty())
+                    .filter(table -> TableStatus.AVAILABLE.equals(table.getStatus()))
+                    .count();
+
+            this.totalTableCount = restaurant.getSections().stream()
+                    .mapToInt(section -> section.getTables() != null ? section.getTables().size() : 0)
+                    .sum();
+        } else {
+            this.freeTableCount = 0;
+            this.totalTableCount = 0;
+        }
     }
 }
